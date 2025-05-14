965fd1dfacb5a8748e097ba59776b04a30d591f7
Joao Neto <joaonetow9@gmail.com>
2025-05-13 21:50:18 -0300

    Finalização da primeira versão

--- script/main.py
+++ script/main.py
@@ -24,34 +24,39 @@ palavras = [
 ]
 
 # Inicio do jogo
+print("")
 print("Bem-vindo(a) ao jogo da forca!")
 print("Adivinhe a palavra abaixo:")
+print("")
+print("")
 
 # Armazena a palavra escolhida aleatoriamente
 palavra_escolhida = rd.choice(palavras)
 
 # Armazena a palavra de forma mascarada
-# palavra_oculta = crialinha(palavra_escolhida)
 palavra_oculta_lista = ["_" for i in palavra_escolhida]
 palavra_oculta = "".join(palavra_oculta_lista)
 print(palavra_oculta)
+print("")
 
 ##########################################
 
 # While onde irá continuar perguntando até que...
 letras_erradas = []
 chances = 6
-while chances != 0:
-
-    # Mostra os pontos do usuário
+while chances > 0:
+    # Mostra a interface do usuário: Chances e Letras erradas.
     print("Chances restantes: {}".format(chances))
-    print("Letras erradas: {}".format(letras_erradas))
+    print("Letras erradas: {}".format(", ".join(letras_erradas)))
+    print("")
+    print("")
 
     # Pede a letra para arriscar e verifica se ela é válida
     letra = str(input("Digite uma letra: ")).lower()
     while (len(letra) == 0) or (len(letra)) > 1 or (letra.isnumeric()):
         print("Você deve digitar apenas uma letra!")
         letra = str(input("Digite uma letra: "))
+        print("")
 
     # Verifica se existe a letra na palavra
     if letra not in palavra_escolhida:
@@ -63,10 +68,21 @@ while chances != 0:
             if caractere == letra:
                 palavra_oculta_lista[indice] = letra
 
-            
+    # Verifica se venceu o desafio, se não venceu, continua...
+    if "".join(palavra_oculta_lista) == palavra_escolhida:
+         print("")
+         print("Você venceu, a palavra era: {}".format(palavra_escolhida))
+         print("")
+         break
+    else:
+        print("".join(palavra_oculta_lista))
+        print("")
+    print("========================================")
 
-            
-    print("".join(palavra_oculta_lista))
+# Mensagem de derrota caso você perca todas as chances
+if chances <= 0:
+     print("Você perdeu, a palavra era: {}".format(palavra_escolhida))
+    
 
 
 
--- script/testes.py
+++ script/testes.py
@@ -1,11 +1,2 @@
-palavra = "computador"
-espacos = ["_" for i in palavra]
-teste = "".join(espacos)
-letra = "o"
-
-for i in palavra:
-    if i == letra:
-        espacos[palavra.index(i)] = i
-
-
-print(espacos)
+with open('arquivos/palavras.txt') as arquivo:
+    arquivo.write()
